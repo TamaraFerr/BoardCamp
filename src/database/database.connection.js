fa3bab6911d29d2182576cc53ebaa001e22020bf
TamaraFerr <tamarablacksmith@hotmail.com>
2023-09-25 15:34:23 -0300

    fix: port connection

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,12 +1,13 @@
 import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const { Pool } = pg;
 
 const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 };
 
-export const db = new Pool(configDatabase);
+const db = new Pool(configDatabase);
+
+export default db;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
-import gamesRoutes from "./games.routes";
-import clientsRoutes from "./clients.routes";
-import rentalsRoutes from "./rentals.routes";
+import gamesRoutes from "./games.routes.js";
+import clientsRoutes from "./clients.routes.js";
+import rentalsRoutes from "./rentals.routes.js";
 
 const router = express.Router();
 
